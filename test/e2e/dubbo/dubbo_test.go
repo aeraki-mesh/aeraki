fda43bbdc9e386aa6ab4f9a900fd9095d4cff9d8
huabingzhao <huabingzhao@tencent.com>
2020-12-14 15:31:22 +0800

    add version routing and percentage routing test
    Signed-off-by: huabingzhao <huabingzhao@tencent.com>

--- test/e2e/dubbo/dubbo_test.go
+++ test/e2e/dubbo/dubbo_test.go
@@ -46,15 +46,57 @@ func shutdown() {
 	util.DeleteNamespace("dubbo", "")
 }
 
-func TestSidecarConfig(t *testing.T) {
+func TestSidecarOutboundConfig(t *testing.T) {
 	util.WaitForDeploymentsReady("dubbo", 10*time.Minute, "")
 	consumerPod, _ := util.GetPodName("dubbo", "app=dubbo-sample-consumer", "")
-	config, _ := util.PodExec("dubbo", consumerPod, "dubbo-sample-consumer", "curl 127.0.0.1:15000/config_dump", false, "")
+	config, _ := util.PodExec("dubbo", consumerPod, "istio-proxy", "curl 127.0.0.1:15000/config_dump", false, "")
 	config = strings.Join(strings.Fields(config), "")
 	want := "{\n\"name\":\"envoy.filters.network.dubbo_proxy\",\n\"typed_config\":{\n\"@type\":\"type.googleapis.com/envoy.extensions.filters.network.dubbo_proxy.v3.DubboProxy\",\n\"stat_prefix\":\"outbound|20880||org.apache.dubbo.samples.basic.api.demoservice\",\n\"route_config\":[\n{\n\"name\":\"outbound|20880||org.apache.dubbo.samples.basic.api.demoservice\",\n\"interface\":\"org.apache.dubbo.samples.basic.api.DemoService\",\n\"routes\":[\n{\n\"match\":{\n\"method\":{\n\"name\":{\n\"safe_regex\":{\n\"google_re2\":{},\n\"regex\":\".*\"\n}\n}\n}\n},\n\"route\":{\n\"cluster\":\"outbound|20880||org.apache.dubbo.samples.basic.api.demoservice\"\n}\n}\n]\n}\n]\n}\n}\n]\n}"
 	want = strings.Join(strings.Fields(want), "")
 	log.Info(config)
 	if !strings.Contains(config, want) {
 		t.Error("cant't find dubbo proxy in the outbound listener of the envoy sidecar")
+	}
+}
+
+func TestVersionRouting(t *testing.T) {
+	util.WaitForDeploymentsReady("dubbo", 10*time.Minute, "")
+	testVersion("v1", t)
+	testVersion("v2", t)
+}
+
+func testVersion(version string, t *testing.T) {
+	util.KubeApply("dubbo", "testdata/virtualservice-"+version+".yaml", "")
+	log.Info("Waiting for rules to propagate ...")
+	time.Sleep(2 * time.Minute)
+	consumerPod, _ := util.GetPodName("dubbo", "app=dubbo-sample-consumer", "")
+	for i := 0; i < 5; i++ {
+		dubboResponse, _ := util.PodExec("dubbo", consumerPod, "dubbo-sample-consumer", "curl 127.0.0.1:9009/hello", false, "")
+		want := "response from dubbo-sample-provider-" + version
+		log.Info(dubboResponse)
+		if !strings.Contains(dubboResponse, want) {
+			t.Error("")
+		}
+	}
+}
+
+func TestPercentageRouting(t *testing.T) {
+	util.WaitForDeploymentsReady("dubbo", 10*time.Minute, "")
+	util.KubeApply("dubbo", "testdata/virtualservice-traffic-splitting.yaml", "")
+	log.Info("Waiting for rules to propagate ...")
+	time.Sleep(2 * time.Minute)
+	consumerPod, _ := util.GetPodName("dubbo", "app=dubbo-sample-consumer", "")
+	v1 := 0
+	for i := 0; i < 20; i++ {
+		dubboResponse, _ := util.PodExec("dubbo", consumerPod, "dubbo-sample-consumer", "curl 127.0.0.1:9009/hello", false, "")
+		responseV1 := "response from dubbo-sample-provider-v1"
+		log.Info(dubboResponse)
+		if strings.Contains(dubboResponse, responseV1) {
+			v1++
+		}
+	}
+	// The most accurate number should be 6, but the number may fall into a range around 6 since the sample is not big enough
+	if v1 > 8 || v1 < 4 {
+		t.Errorf("percentage traffic routing failed, want: %v got:%v ", 3, v1)
 	}
 }
