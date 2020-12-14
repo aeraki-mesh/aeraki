18478bae9a39bbd57dcc4b82e8f9f54e3bff35a5
huabingzhao <huabingzhao@tencent.com>
2020-12-14 15:31:22 +0800

    add version routing and percentage routing test
    Signed-off-by: huabingzhao <huabingzhao@tencent.com>

--- test/e2e/scripts/aeraki.sh
+++ test/e2e/scripts/aeraki.sh
@@ -4,4 +4,5 @@ set -ex
 
 export BUILD_TAG=`git log --format="%H" -n 1`
 sed "s/\${BUILD_TAG}/$BUILD_TAG/" test/e2e/common/aeraki.yaml > aeraki.yaml
+kubectl create ns istio-system
 kubectl apply -f aeraki.yaml -n istio-system
