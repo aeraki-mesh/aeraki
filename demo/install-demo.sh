27ec4c55211a5aafb0c5589b17f74a2fc4ea776d
zhaohuabing <huabingzhao@tencent.com>
2021-01-06 15:38:24 +0800

    use kaili operator to install kaili
    Signed-off-by: zhaohuabing <huabingzhao@tencent.com>

--- demo/install-demo.sh
+++ demo/install-demo.sh
@@ -8,9 +8,17 @@ export BUILD_TAG=latest
 bash ${SCRIPTS_DIR}/aeraki.sh
 bash ${SCRIPTS_DIR}/istio.sh -y -f ${COMMON_DIR}/istio-config.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/prometheus.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/grafana.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/prometheus.yaml -n istio-system
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/grafana.yaml -n istio-system	
+
+kubectl create namespace kiali-operator
+helm install \
+    --set cr.create=true \
+    --set cr.namespace=istio-system \
+    --namespace kiali-operator \
+    --repo https://kiali.org/helm-charts \
+    kiali-operator \
+    kiali-operator
 
 kubectl apply -f $BASEDIR/demo/gateway/demo-ingress.yaml -n istio-system
 kubectl apply -f $BASEDIR/demo/gateway/istio-ingressgateway.yaml -n istio-system
--- demo/uninstall-demo.sh
+++ demo/uninstall-demo.sh
@@ -1,8 +1,8 @@
 BASEDIR=$(dirname "$0")/..
 
-kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/prometheus.yaml
-kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/grafana.yaml
-kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/kiali.yaml
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/prometheus.yaml -n istio-system
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/grafana.yaml -n istio-system
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/kiali.yaml -n istio-system
 
 kubectl delete -f $BASEDIR/demo/gateway/demo-ingress.yaml -n istio-system
 kubectl delete -f $BASEDIR/demo/gateway/istio-ingressgateway.yaml -n istio-system
@@ -11,4 +11,5 @@ bash $BASEDIR/demo/dubbo/uninstall.sh
 bash $BASEDIR/demo/thrift/uninstall.sh
 bash ${BASEDIR}/demo/kafka/uninstall.sh
 
+kubectl delete kiali kiali -n istio-system
 kubectl delete ns istio-system
