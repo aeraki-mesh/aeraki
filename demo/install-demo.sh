a2686631e9b0659b9d78322735d6517d9ebd2751
zhaohuabing <huabingzhao@tencent.com>
2021-10-11 16:54:06 +0800

    update demo script
    Signed-off-by: zhaohuabing <huabingzhao@tencent.com>

--- demo/install-demo.sh
+++ demo/install-demo.sh
@@ -5,11 +5,10 @@ COMMON_DIR=$BASEDIR/test/e2e/common
 export ISTIO_VERSION=1.10.0
 export BUILD_TAG=latest
 
-bash ${SCRIPTS_DIR}/aeraki.sh
 bash ${SCRIPTS_DIR}/istio.sh -y -f ${COMMON_DIR}/istio-config.yaml
+bash ${SCRIPTS_DIR}/aeraki.sh
 
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/prometheus.yaml -n \
-istio-system
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/prometheus.yaml -n istio-system
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/grafana.yaml -n istio-system
 
 kubectl create namespace kiali-operator
@@ -24,6 +23,9 @@ helm install \
 kubectl apply -f $BASEDIR/demo/gateway/demo-ingress.yaml -n istio-system
 kubectl apply -f $BASEDIR/demo/gateway/istio-ingressgateway.yaml -n istio-system
 
-bash $BASEDIR/demo/metaprotocol-dubbo/install.sh
-bash $BASEDIR/demo/thrift/install.sh
-bash ${BASEDIR}/demo/kafka/install.sh
+if [ $? -ne 0 ]
+then
+    bash $BASEDIR/demo/metaprotocol-dubbo/install.sh
+    bash $BASEDIR/demo/thrift/install.sh
+    bash ${BASEDIR}/demo/kafka/install.sh
+fi
