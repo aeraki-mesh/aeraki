aff51d2f092a258a75caeed7fe0cec055dec1f25
Xunzhuo <mixdeers@gmail.com>
2021-12-04 16:25:07 +0800

    fix: run tidy before e2e test and update lazyxds.sh (#130)
    * chore: update lazyxds.sh and remove tmp controller.yaml
    
    * fix: format yaml style
    
    * fix: go mod tidy before building

--- test/e2e/scripts/lazyxds.sh
+++ test/e2e/scripts/lazyxds.sh
@@ -3,11 +3,16 @@
 set -ex
 BASEDIR=$(dirname "$0")
 
+TMP_DIR=$BASEDIR/../tmp
+
 if [ -z "$BUILD_TAG" ]
 then
   export BUILD_TAG=`git log --format="%H" -n 1`
 fi
 
-envsubst < $BASEDIR/../common/lazyxds-controller.yaml > lazyxds-controller.yaml
-kubectl apply -f lazyxds-controller.yaml
+rm -rf $TMP_DIR
+mkdir $TMP_DIR
+envsubst < $BASEDIR/../common/lazyxds-controller.yaml > $TMP_DIR/lazyxds-controller-tmp.yaml
+kubectl apply -f $TMP_DIR/lazyxds-controller-tmp.yaml
 kubectl apply -f $BASEDIR/../../../lazyxds/install/lazyxds-egress.yaml
+rm -rf $TMP_DIR
