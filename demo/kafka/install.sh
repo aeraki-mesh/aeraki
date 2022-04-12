211ae7b3f456f67be1488cc7b7ef45f5487a0698
zhaohuabing <zhaohuabing@gmail.com>
2022-04-12 16:58:21 +0800

    fix helm repo (#190)
    fix: https://github.com/aeraki-mesh/aeraki/issues/188
    
    Signed-off-by: huabingzhao <zhaohuabing@gmail.com>

--- demo/kafka/install.sh
+++ demo/kafka/install.sh
@@ -16,7 +16,7 @@ BASEDIR=$(dirname "$0")
 
 kubectl create ns kafka
 kubectl label namespace kafka istio-injection=enabled --overwrite=true
-helm repo add zhaohuabing http://zhaohuabing.com/helm-repo/
+helm repo add zhaohuabing https://zhaohuabing.github.io/helm-repo
 helm repo update
 helm install my-release --set persistence.enabled=false --set zookeeper.persistence.enabled=false zhaohuabing/kafka -n kafka
 kubectl apply -f $BASEDIR/kafka-sample.yaml -n kafka
