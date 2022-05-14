e8dafb527df72ab5f8377a38ebcfd4873dce095f
Kenshin Chen <smwyzi@qq.com>
2022-05-14 19:51:10 +0800

    add brpc demo (#218)

--- demo/uninstall-demo.sh
+++ demo/uninstall-demo.sh
@@ -14,15 +14,26 @@
 
 BASEDIR=$(dirname "$0")/..
 
+DEMO=$1
+
 kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/prometheus.yaml -n istio-system
 kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/grafana.yaml -n istio-system
 
 kubectl delete -f $BASEDIR/demo/gateway/demo-ingress.yaml -n istio-system
 kubectl delete -f $BASEDIR/demo/gateway/istio-ingressgateway.yaml -n istio-system
 
-bash $BASEDIR/demo/metaprotocol-dubbo/uninstall.sh
-bash $BASEDIR/demo/metaprotocol-thrift/uninstall.sh
-bash ${BASEDIR}/demo/kafka/uninstall.sh
+
+if [ ${DEMO} == "default" ]
+then
+    bash ${BASEDIR}/demo/metaprotocol-dubbo/uninstall.sh
+    bash ${BASEDIR}/demo/metaprotocol-thrift/uninstall.sh
+elif [ ${DEMO} == "brpc" ]
+then
+    bash ${BASEDIR}/demo/metaprotocol-brpc/uninstall.sh
+elif [ ${DEMO} == "kafka" ]
+then
+    bash ${BASEDIR}/demo/kafka/uninstall.sh
+fi
 
 kubectl delete kiali kiali -n istio-system
 kubectl delete ns istio-system
