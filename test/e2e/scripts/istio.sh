c859bd160fe14fd321d15f47ec86cf8f11345eda
huabingzhao <zhaohuabing@gmail.com>
2022-11-18 17:00:24 +0800

    update istio to 1.14.5
    Signed-off-by: huabingzhao <zhaohuabing@gmail.com>

--- test/e2e/scripts/istio.sh
+++ test/e2e/scripts/istio.sh
@@ -22,7 +22,7 @@
 
 set -ex
 
-istioctl version || (curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION  sh - && sudo mv $PWD/istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin/)
+[ -n "$(istioctl version --remote=false |grep $ISTIO_VERSION)" ] || (curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION  sh - && sudo mv $PWD/istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin/)
 kubectl create ns istio-system || true
 istioctl install $@
 kubectl label namespace default istio-injection=enabled --overwrite=true
