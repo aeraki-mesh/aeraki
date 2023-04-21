d7c0f383ca12137a55cb4bbe06bac5fa60a19d46
panniyuyu@gmail.com <panniyuyu@gmail.com>
2023-04-21 16:39:07 +0800

    Feat: Automatically create vs when Aeraki is used as a gateway.
    Signed-off-by: panniyuyu@gmail.com <panniyuyu@gmail.com>

--- test/e2e/metaprotocolgateway/metaprotocol_gateway_test.go
+++ test/e2e/metaprotocolgateway/metaprotocol_gateway_test.go
@@ -40,7 +40,6 @@ func setup() {
 	util.KubeApply("istio-system", "../../../k8s/aeraki-bootstrap-config.yaml", "")
 	util.KubeApply("meta-thrift", "testdata/thrift-sample.yaml", "")    // thrift server/svc
 	util.KubeApply("istio-system", "testdata/ingress-gateway.yaml", "") // ingress gateway
-	util.KubeApply("meta-thrift", "testdata/virtualservice.yaml", "")
 	util.KubeApply("meta-thrift", "testdata/destinationrule.yaml", "")
 	util.KubeApply("meta-thrift", "testdata/metarouter.yaml", "")
 }
