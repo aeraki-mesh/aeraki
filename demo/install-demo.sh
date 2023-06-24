d91740ea245bac729e231eb4bc268cfbcda782ce
huabing zhao <zhaohuabing@gmail.com>
2023-06-24 20:46:06 +0800

    update aeraki to 1.3.0
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- demo/install-demo.sh
+++ demo/install-demo.sh
@@ -21,7 +21,7 @@ DEMO=$1
 SCRIPTS_DIR=$BASEDIR/test/e2e/scripts
 
 if [ -z "$AERAKI_TAG" ]; then
-  export AERAKI_TAG="1.2.3"
+  export AERAKI_TAG="1.3.0"
 fi
 bash ${SCRIPTS_DIR}/istio.sh
 bash ${SCRIPTS_DIR}/addons.sh
